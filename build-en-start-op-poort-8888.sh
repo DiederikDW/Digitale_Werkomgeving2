ffaebbae76ae50413a2586b64f8cf56782464817
DiederikDW <237992929+DiederikDW@users.noreply.github.com>
2026-05-17 17:43:17 +0200

    build optie -t toegevoegd

--- build-en-start-op-poort-8888.sh
+++ build-en-start-op-poort-8888.sh
@@ -2,4 +2,4 @@
 
 docker build --no-cache -t test-docker-image .
 
-docker run -d --name test-docker-container -p 8888:80 test-docker-image
+docker run -d -t --name test-docker-container -p 8888:80 test-docker-image
